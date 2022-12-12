3c28776fe64ece5bd6f2e0bb34416c71c00657e5
husnafazal <h.ahmed1@alustudent.com>
2022-12-12 05:01:14 -0800

    added 0-sky_Is_the_Limit_not.pp

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# increasing the file limit 
+exec {'increase max limit':
+  command => 'sed -i "s|15|20000|g" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
---
+++ web_stack_debugging_4/README.md
@@ -0,0 +1,2 @@
+0. Sky is the limit, let's bring that limit higher
+Increasing the file limit 
