151cd990d268174ff4558d72a33706354bef91b2
husnafazal <h.ahmed1@alustudent.com>
2022-12-12 05:21:30 -0800

    mades ome chages

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # increasing the file limit 
 exec {'increase max limit':
-  command =>  'sed -i "s/15/20000/g" /etc/default/nginx;service nginx restart',
-    path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+	command =>  'sed -i "s/15/20000/g" /etc/default/nginx;service nginx restart',
+	path    =>  '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
